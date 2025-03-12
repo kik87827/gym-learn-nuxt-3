0372076d85abd970b68aedd14a27dd5a4e3fe536
kik <kik87827@gmail.com>
2025-03-12 18:21:46 +0900

    스터디 진행중 0312

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,13 +1,21 @@
 import type { CourseWithPath } from '~/types/course';
 
 interface CouseReturn {
-  course: Maybe<CourseWithPath>;
+  course: Maybe<CourseWithPath>; // Course | null | undefined
+  prevCourse: Maybe<CourseWithPath>;
+  nextCourse: Maybe<CourseWithPath>;
 }
 
 export const useCourse = (courseSlug: string): CouseReturn => {
   const { courses } = useCourses();
-  const course = courses.find((course) => course.courseSlug === courseSlug);
+  // const course = courses.find((course) => course.courseSlug === courseSlug);
+  const index = courses.findIndex((course) => course.courseSlug === courseSlug);
+  const course = courses[index];
+  const prevCourse = index <= 0 ? null : courses[index - 1];
+  const nextCourse = index >= courses.length - 1 ? null : courses[index + 1];
   return {
     course,
+    prevCourse,
+    nextCourse,
   };
 };
